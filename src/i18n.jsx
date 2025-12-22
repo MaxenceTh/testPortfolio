5ea2e39ef011bbb8736d92be6137b9b95a8d9c21
MaxenceTh <thomas.maxence141v2@gmail.com>
2025-12-22 16:25:12 +0100

    langue

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,12 +1,12 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
+// import LanguageDetector from 'i18next-browser-languagedetector'
 
 import fr from './trad/fr.json'
 import en from './trad/en.json'
 
 i18n
-  .use(LanguageDetector) // détecte la langue du navigateur
+  // .use(LanguageDetector) // détecte la langue du navigateur
   .use(initReactI18next)
   .init({
     resources: {
